0fb1d7882d8b65abec8610b1a1540f4aec7facab
notivalenciaaldia <orlandoflorez1169@gmail.com>
2026-05-11 10:58:02 -0500

    Add files via upload

---
+++ valencia.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>GLAC</title>
+    <link rel="stylesheet" href="CSS">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+<h1 class="text-center">FUNDACION NUEVO RENACER</h1>
+<h2 class="text-center">GRUPOS LOCALES DE AHORRO Y CREDITO</h2>
+<div class="container">
+
+    <!-- Municipio Tierralta o Valencia -->
+    <div class="card">
+        <h2 class="text-center">FACILITADORES DE VALENCIA</h2>
+    </div>
+    <div class="COL-8 P-4">
+        <table class="table">
+  <thead class="table">
+    <tr>
+      <th scope="col">NUMERO</th>
+      <th scope="col">NOMBRES</th>
+      <th scope="col">APELLIDOS</th>
+      <th scope="col">DOCUMENTO</th>
+      <th scope="col">VER FACILITADOR</th>
+    </tr>
+  </thead>
+   <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+     <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      
+    </tr>
+  </tbody>
+</table>
+    
+  </div>
+
+
+<script src="app.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
